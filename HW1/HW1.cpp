a908052374f89f5609313fa09dde3048a4ad93d7
bogdandelcea <148143648+bogdandelcea@users.noreply.github.com>
2023-11-19 22:07:32 +0200

    Add files via upload

---
+++ HW1/HW1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+#include "b.h"
+
+int main()
+{
+	test();
+	testb();
+	return 0;
+}
---
+++ HW1/b.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "b.h"
+
+void testb() {
+    cout << "Testing from library B" << '\n';
+    test();
+}
+
